daa0ea60bbef2cfc8503dd278bdd7f9aeb559307
Verdier <ldverdier@gmail.com>
2023-02-20 22:15:07 +0100

    feat: implemented first version of Liquidity

--- grpc-server/src/modules/swap/swap.controller.ts
+++ grpc-server/src/modules/swap/swap.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 import { LiquidityRequest__Output } from 'src/grpc/interfaces/swap/LiquidityRequest';
+import { TradingPair } from 'src/grpc/interfaces/swap/TradingPair';
 import { TradingPairListResponse } from 'src/grpc/interfaces/swap/TradingPairListResponse';
 
 @Controller()
@@ -27,5 +29,29 @@ export class SwapController {
     };
 
     return pairs;
+  }
+
+  @GrpcMethod('SwapService')
+  liquidity(data: LiquidityRequest__Output): Observable<TradingPair> {
+    const tradingPairs$ = new Observable<TradingPair>((subscriber) => {
+      data.tokens.forEach((token) => {
+        const pair: TradingPair = {
+          a: {
+            name: token,
+            amount: 1,
+            policy: `policy for ${token}`,
+          },
+          b: {
+            name: 'random token',
+            amount: 2,
+            policy: `policy for random token`,
+          },
+        };
+        subscriber.next(pair);
+      });
+      subscriber.complete();
+    });
+
+    return tradingPairs$;
   }
 }
